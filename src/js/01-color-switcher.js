9866ec7a05a53ce525c8ba489529b49d47772120
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2023-11-28 19:39:59 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStop.setAttribute('disabled', 'disabled');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let timerId = null;
+function changeColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
+
+btnStart.addEventListener('click', changeColor);
+btnStop.addEventListener('click', stopChangeColor);
